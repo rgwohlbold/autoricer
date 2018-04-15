58d4ab2391a0f2bd5a5f142752e983353e03d982
Richard Wohlbold <rigewo02@gmail.com>
2018-04-15 15:16:27 -0500

    Improved logging and fish config

--- ricer.sh
+++ ricer.sh
@@ -5,39 +5,35 @@ if [ ! "$(whoami)" = "root" ] || [ "$SUDO_USER" = "" ]; then
     exit
 fi
 
-log() {
-    echo "$1"
-}
-
 install_pkg() {
     res=$(pacman -Q make >/dev/null 2>&1)
     if [ $? = 1 ]; then
-        log "Installing base-devel"
+        echo "Installing base-devel..." >&2
         pacman -S base-devel --noconfirm
     fi
 
-    log "Installing yay AUR helper"
+    echo "Installing yay AUR helper..." >&2
     # Install yay AUR helper
     sudo -u "$SUDO_USER" git clone https://aur.archlinux.org/yay
     cd yay
     echo $SUDO_USER
-    sudo -u "$SUDO_USER" makepkg -si --noconfirm
+    sudo -u "$SUDO_USER" makepkg -si --noconfirm --quiet
     cd ..
     rm -rf yay/
 
     # TODO add MAKEFLAGS=-j5 to makepkg.conf
 
-    log "Installing packages..."
+    echo "Installing basic packages..." >&2
     # Install packages
-    sudo -u "$SUDO_USER" yay -S --needed --noconfirm --sudoloop - < packages.txt
-    sudo -u "$SUDO_USER" yay -R --noconfirm --sudoloop rxvt-unicode
-    sudo -u "$SUDO_USER" yay -S --noconfirm --sudoloop rxvt-unicode-patched
+    sudo -u "$SUDO_USER" yay -S --quiet --needed --noconfirm --sudoloop - < packages.txt
+    sudo -u "$SUDO_USER" yay -R --quiet --noconfirm --sudoloop rxvt-unicode
+    sudo -u "$SUDO_USER" yay -S --quiet --noconfirm --sudoloop rxvt-unicode-patched
 
     # Install extra packages if user wants it
     case "$1" in
         *[yY])
-            log "Installing extra packages..."
-            yay -S --noconfirm --sudoloop - < extra.txt | tee realout
+            echo "Installing extra packages..." >&2
+            yay -S --quiet --noconfirm --sudoloop - < extra.txt | tee realout
             ;;
     esac
 }
@@ -46,13 +42,13 @@ install_pkg() {
 config_xorg() {
     # If using intel drivers, use TearFree option
     if [ "$1" = "y" ]; then
-        log "Installing intel drivers"
+        echo "Installing intel drivers" >&2
         cp ./20-intel.conf /etc/X11/xorg.conf.d/
     fi
 }
 
 config_lock() {
-    log "Installing lock screen..."
+    echo "Installing lock screen..." >&2
     if [ -f /usr/bin/xflock4 ]; then
         rm /usr/bin/xflock4
     fi
@@ -61,16 +57,12 @@ config_lock() {
 
 
 main() {
-    #install_pkg "$1"
+    install_pkg "$1"
     config_xorg "$2"
     config_lock
 }
 
-mkfifo realout
-cat realout &
-
-read -p "Install extra packages? " extra
-read -p "Intel drivers? " intel
-main "$extra" "$intel"
+read -p "Install extra packages? (y/n) " extra
+read -p "Intel drivers? (y/n) " intel
+main "$extra" "$intel" | sudo -u "$SUDO_USER" tee out.log
 sudo -u "$SUDO_USER" ./unprivileged.sh
-rm realout
