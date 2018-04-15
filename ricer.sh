52760224510bd8931a25cf2c0f12cb9a003ba3bf
Richard Wohlbold <rigewo02@gmail.com>
2018-04-15 16:25:12 -0500

    Refactored script, added ack to packages and moved vscode to extra

--- ricer.sh
+++ ricer.sh
@@ -5,45 +5,78 @@ if [ "$(whoami)" = "root" ]; then
     exit
 fi
 
+
+install_pkg() {
+    if [ $(pacman -Q make >/dev/null 2>&1) ]; then
+        sudo pacman -S base-devel --noconfirm
+    fi
+
+    # Install yay AUR helper
+    git clone https://aur.archlinux.org/yay
+    cd yay
+    makepkg -si --noconfirm
+    cd ..
+    rm -rf yay/
+
+    # Install packages
+    yay -S --noconfirm - < packages.txt
+    yay -R --noconfirm rxvt-unicode
+    yay -S --noconfirm rxvt-unicode-patched
+
+    # Install extra packages if user wants it
+    case "$1" in
+        *[yY])
+            yay -S --noconfirm - < extra.txt
+            ;;
+    esac
+}
+
+config_xorg(){
+    # Make Xorg configurations
+    cp ./.xinitrc ./.Xresources "$home"
+    sudo cp ./20-intel.conf /etc/X11/xorg.conf.d/
+}
+
+config_i3() {
+    # Make i3-gaps configuration
+    rm -rf "$home/.config/i3/"
+    cp -r ./i3/ "$home/.config/i3/"
+}
+
+
+vimplugin() {
+    git clone "$1" "~/.vim/bundle/$2"
+}
+
+config_vim() {
+    # Install pathogen and make vim configuration
+    mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+    cp ./.vimrc "$home"
+
+    # Install vim plugins
+    vimplugin https://github.com/mileszs/ack.vim ack.vim/    
+    vimplugin https://github.com/junegunn/goyo.vim goyo.vim/      
+    vimplugin https://github.com/junegunn/limelight.vim limelight.vim/  
+    vimplugin https://github.com/majutsushi/tagbar tagbar/       
+    vimplugin https://github.com/michaeljsmith/vim-indent-object vim-indent-object/
+    vimplugin https://github.com/kien/ctrlp.vim ctrlp.vim/  
+    vimplugin https://github.com/mboughaba/i3config.vim i3config.vim/  
+    vimplugin https://github.com/scrooloose/nerdtree nerdtree/       
+    vimplugin https://github.com/vim-airline/vim-airline vim-airline/  
+    vimplugin https://github.com/tpope/vim-commentary vim-commentary/      
+    vimplugin https://github.com/tpope/vim-surround vim-surround/
+    curl https://raw.githubusercontent.com/NLKNguyen/papercolor-theme/master/colors/PaperColor.vim > ~/.vim/colors/ 
+
+}
+
+
 read -p "Install extra packages? " extra
 sudo touch /tmp/hello
 sudo rm /tmp/hello
 
 home="$HOME"
-
-if [ $(pacman -Q make >/dev/null 2>&1) ]; then
-    sudo pacman -S base-devel --noconfirm
-fi
-
-# Install yay AUR helper
-git clone https://aur.archlinux.org/yay
-cd yay
-makepkg -si --noconfirm
-cd ..
-rm -rf yay/
-
-# Install packages
-yay -S --noconfirm - < packages.txt
-yay -R --noconfirm rxvt-unicode
-yay -S --noconfirm rxvt-unicode-patched
-
-# Install extra packages if user wants it
-case $extra in
-    *[yY])
-        yay -S --noconfirm - < extra.txt
-        ;;
-esac
-
-# Make Xorg configurations
-cp ./.xinitrc ./.Xresources "$home"
-cp ./20-intel.conf /etc/X11/xorg.conf.d/
-
-# Make i3-gaps configuration
-rm -rf "$home/.config/i3/"
-cp -r ./i3/ "$home/.config/i3/"
-
-# Install pathogen and make vim configuration
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-cp ./.vimrc "$home"
-
+install_pkg "$extra"
+config_xorg "$home"
+config_i3 "$home"
+config_vim "$home"
