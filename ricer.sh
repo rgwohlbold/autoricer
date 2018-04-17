38d7dfb2abb5631a7e91ddd2fc32144f260346c0
Richard Wohlbold <rigewo02@gmail.com>
2018-04-17 14:26:01 -0500

    Minor fixes

--- ricer.sh
+++ ricer.sh
@@ -6,6 +6,11 @@ if [ ! "$(whoami)" = "root" ] || [ "$SUDO_USER" = "" ]; then
 fi
 
 install_pkg() {
+    # Update packacke database
+    echo "Synchronizing package databases..."
+    pacman -Syy 2>&1
+
+    # Install base-devel if not yet installed
     res=$(pacman -Q make >/dev/null 2>&1)
     if [ $? = 1 ]; then
         echo "Installing base-devel..." >&2
@@ -17,18 +22,19 @@ install_pkg() {
     threads=$(( $(nproc --all) + 1))
     sed "s/-j5/-j${threads}/g" ./pkg/makepkg.conf > /etc/makepkg.conf
 
-    echo "Installing yay AUR helper..." >&2
     # Install yay AUR helper
+    echo "Installing yay AUR helper..." >&2
     sudo -u "$SUDO_USER" git clone -q https://aur.archlinux.org/yay || (echo "Failed to install yay, exiting..." >&2 && exit 1)
     cd yay
     sudo -u "$SUDO_USER" makepkg -si --noconfirm 2>&1 || (echo "Failed to install yay, exiting..." >&2 && exit 1)
     cd ..
     rm -rf yay/
 
-
+    # Install basic packages
     echo "Installing basic packages..." >&2
-    # Install packages
-    sudo -u "$SUDO_USER" yay -S --quiet --needed --noconfirm --sudoloop - < ./pkg/packages.txt 2>&1 || (echo "Failed to install basic packages, exiting..." >&2 && exit 1)
+    sudo -u "$SUDO_USER" yay -S --quiet --needed --noconfirm --sudoloop - < ./pkg/packages.txt 2>&1 || (echo -e "Failed to install basic packages, exiting...\nYou may want to update your mirrorlist" >&2 && exit 1)
+
+    # Install rxvt-unicode-patched
     if sudo -u "$SUDO_USER" yay -R --quiet --noconfirm --sudoloop rxvt-unicode 2>&1; then
         sudo -u "$SUDO_USER" yay -S --quiet --noconfirm --sudoloop rxvt-unicode-patched 2>&1
     else
@@ -61,11 +67,17 @@ config_lock() {
     cp ./scripts/* /usr/local/bin/
 }
 
+config_shell() {
+    echo "Setting fish as default shell..." >&2
+    chsh -s /usr/bin/fish "$SUDO_USER"
+}
+
 
 main() {
     install_pkg "$1"
     config_xorg "$2"
     config_lock
+    config_shell
 }
 
 read -p "Install extra packages? (y/n) " extra
