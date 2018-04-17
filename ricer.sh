186f8e23f391b7a46a4e8d667fae25d483ecdbbf
Richard Wohlbold <rigewo02@gmail.com>
2018-04-17 11:28:24 -0500

    Added MAKEFLAGS and improved logging

--- ricer.sh
+++ ricer.sh
@@ -9,33 +9,37 @@ install_pkg() {
     res=$(pacman -Q make >/dev/null 2>&1)
     if [ $? = 1 ]; then
         echo "Installing base-devel..." >&2
-        pacman -S base-devel --noconfirm
+        pacman -S base-devel --noconfirm 2>&1 || echo "Failed to install base-devel, exiting..." && exit 1
     fi
 
-    echo "Installing yay AUR helper..." >&2
-    # Install yay AUR helper
-    sudo -u "$SUDO_USER" git clone https://aur.archlinux.org/yay
-    cd yay
-    echo $SUDO_USER
-    sudo -u "$SUDO_USER" makepkg -si --noconfirm --quiet
-    cd ..
-    rm -rf yay/
-
     # Set makeflags to -j{number of cores + 1}
+    echo "Configuring makepkg..." >&2
     threads=$(( $(nproc --all) + 1))
     sed "s/-j5/-j${threads}/g" ./pkg/makepkg.conf > /etc/makepkg.conf
 
+    echo "Installing yay AUR helper..." >&2
+    # Install yay AUR helper
+    sudo -u "$SUDO_USER" git clone -q https://aur.archlinux.org/yay || echo "Failed to install yay, exiting..." >&2 && exit 1
+    cd yay
+    sudo -u "$SUDO_USER" makepkg -si --noconfirm 2>&1 || echo "Failed to install yay, exiting..." >&2 && exit 1
+    cd ..
+    rm -rf yay/
+
+
     echo "Installing basic packages..." >&2
     # Install packages
-    sudo -u "$SUDO_USER" yay -S --quiet --needed --noconfirm --sudoloop - < ./pkg/packages.txt
-    sudo -u "$SUDO_USER" yay -R --quiet --noconfirm --sudoloop rxvt-unicode
-    sudo -u "$SUDO_USER" yay -S --quiet --noconfirm --sudoloop rxvt-unicode-patched
+    sudo -u "$SUDO_USER" yay -S --quiet --needed --noconfirm --sudoloop - < ./pkg/packages.txt 2>&1 || echo "Failed to install basic packages, exiting..." >&2 && exit 1
+    if sudo -u "$SUDO_USER" yay -R --quiet --noconfirm --sudoloop rxvt-unicode 2>&1; then
+        sudo -u "$SUDO_USER" yay -S --quiet --noconfirm --sudoloop rxvt-unicode-patched 2>&1
+    else
+        echo "Failed to remove rxvt-unicode, terminal will not have fixed font spacing" >&2
+    fi
 
     # Install extra packages if user wants it
     case "$1" in
         *[yY])
             echo "Installing extra packages..." >&2
-            yay -S --quiet --noconfirm --sudoloop - < ./pkg/extra.txt
+            yay -S --quiet --noconfirm --sudoloop - < ./pkg/extra.txt 2>&1 || echo "Failed to install extra packages, continuing..." >&2
             ;;
     esac
 }
